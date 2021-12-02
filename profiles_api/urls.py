d1b7e4d21e17e5b2270422ea87858ab2f05090e8
Aparna <aparnavenugopal1996@gmail.com>
2021-12-02 17:25:20 +0530

    added profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,7 @@ from profiles_api import views
 
 router=DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
+router.register('profile',views.UserProfileViewSet)
 
 
 
@@ -12,5 +13,6 @@ router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('',include(router.urls)),
+
 
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
-
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     '''test api view'''
@@ -75,3 +77,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self,request,pk=None):
         '''handle deleting an object by its id'''
         return Response({'http_method':'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    '''handle creating and updating profiles'''
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.UpdateOwnProfile,)
